3ba6db37b53899a2b4087c8442cd27ea0265b45a
Aman <aaman10@ms.ds.uhc.com>
2017-11-30 22:03:00 +0000

    mac transfer

--- gapminder.R
+++ gapminder.R
@@ -1,4 +1,6 @@
 library(gapminder)
+library(tidyverse)
+library(factoextra)
 
 clusters <- kmeans(
   x = gapminder[, 4:6], 
@@ -28,3 +30,45 @@ table(
 fviz_nbclust( gapminder[, 4:6], kmeans, method = "wss")
 fviz_nbclust( gapminder[, 4:6], kmeans, method = "silhouette")
 fviz_nbclust( gapminder[, 4:6], kmeans, nstart = 25, method = "gap_stat", nboot = 50)
+
+
+
+
+#Start here
+
+by_year <- gapminder %>%
+  filter(year == 2007)
+
+score <- by_year %>%
+  mutate( score = lifeExp * gdpPercap)
+
+
+clusters <- kmeans(
+  x = by_year[, 4:6], 
+  centers = 2, 
+  nstart = 10)
+
+# Plot each cluster as a shape
+plot(
+  x = by_year$lifeExp, 
+  y = by_year$gdpPercap, 
+  col = as.numeric(by_year$continent), 
+  pch = clusters$cluster)
+
+# Plot centroid of clusters
+points(
+  x = clusters$centers[, "lifeExp"], 
+  y = clusters$centers[, "gdpPercap"],
+  pch = 4, 
+  lwd = 4, 
+  col = "blue")
+
+# View a table of the clusters
+table(
+  x = clusters$cluster, 
+  y = gapminder$continent)
+
+fviz_nbclust( score[, 4:7], kmeans, method = "wss")
+fviz_nbclust( score[, 4:7], kmeans, method = "silhouette")
+fviz_nbclust( score[, 4:7], kmeans, nstart = 25, method = "gap_stat", nboot = 50)
+
